a7cef6e2ec7f4e06cb65f076f9ab375067f65848
bigbisson <bigmasterindra@yahoo.com>
2022-07-29 10:05:08 +0700

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,13 +13,17 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    RouterModule.forRoot([{ path: '', component: ProductListComponent }]),
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+      { path: 'products/:productId', component: ProductDetailsComponent },
+    ]),
   ],
   declarations: [
     AppComponent,
     TopBarComponent,
     ProductListComponent,
     ProductAlertsComponent,
+    ProductDetailsComponent,
   ],
   bootstrap: [AppComponent],
 })
